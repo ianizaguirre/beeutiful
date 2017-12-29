c738cc35489c718dc823c581df299d6595eb4574
[ianizaguirre] <ian@izaguir.re>
2017-12-28 20:44:47 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,44 @@
+
+
+
+// Fetch Current Date
+const date = new Date();
+const fetchDate = date.toDateString();
+document.getElementById('todays-date').innerHTML = fetchDate;
+
+
+
+// Counter
+let i = 0;
+
+// Tracker -- tracks changing state of current forager bee count 
+const getCurrentBeeCount = i;
+
+
+/*
+
+// Sensor 1 
+// Exit Sensor  
+const tripExitSensor = // Sensor 1 via Node JS backend = i--
+
+
+// Sensor 2
+// Entrance Sensor
+const tripEntranceSensor =  // Sensor 2 via Node JS backend = i++
+
+
+function startProgram() {
+	
+// Display getCurrentBeeCount
+
+	if ( tripExitSensor ) {
+		// Chart JS - Graph a Decrease in forager  bee count total
+		// Display getCurrentBeeCount
+	} else if ( tripEntranceSensor ) {
+		// Chart JS - Graph an Increase in forager  bee count total
+		// Display getCurrentBeeCount
+	}
+
+}
+
+*/
